a8fad6cbdd6768bc067b06c7f96b5a021a947ac0
Joerg Steffens <joerg.steffens@bareos.com>
2020-06-08 10:10:49 +0200

    Update setup.py
    Add missing information, fix requirements, update version to 0.3.dev1.

--- setup.py
+++ setup.py
@@ -3,24 +3,34 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='bareos-fuse',
-    version='0.2',
-    author='Joerg Steffens',
-    author_email='joerg.steffens@bareos.com',
+    name="bareos-fuse",
+    #version="0.3.dev2",
+    version="0.3",
+    license="AGPLv3",
+    author="Joerg Steffens",
+    author_email="joerg.steffens@bareos.com",
     #packages=find_packages(),
     package_dir = { 
-        'bareos.fuse': 'bareos/fuse',
-        'bareos.fuse.node': 'bareos/fuse/node',
+        "bareos.fuse": "bareos/fuse",
+        "bareos.fuse.node": "bareos/fuse/node",
     },
-    packages=['bareos.fuse', 'bareos.fuse.node'],
-    scripts=['bin/bareos-fuse.py'],
-    url='https://github.com/bareos/bareos-fuse/',
-    # What does your project relate to?
-    keywords='bareos fuse filesystem',
-    description='Virtual Filesystem Showing Bareos Information.',
-    long_description=open('README.rst').read(),
+    packages=["bareos.fuse", "bareos.fuse.node"],
+    scripts=["bin/bareos-fuse.py"],
+    url="https://github.com/bareos/bareos-fuse/",
+    keywords="bareos fuse filesystem bareosfs",
+    description="Virtual Filesystem showing Bareos information.",
+    long_description=open("README.rst").read(),
+    long_description_content_type='text/x-rst',
     install_requires=[
-        'python-bareos',
-        'python-fuse'
-    ]
+        "fuse-python",
+        "python-bareos",
+        "python-dateutil",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: System :: Archiving :: Backup",
+    ],
 )
